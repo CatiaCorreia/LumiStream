16914fb2ee5b5e65b904d44000a5ad78e2cc7b5f
up202207803 <up202207803@fc.up.pt>
2024-11-22 14:06:49 +0000

    added jdbc to gradle build and Movie class

---
+++ src/main/java/com/lumistream/jersey/movies/Movie.java
@@ -0,0 +1,15 @@
+package com.lumistream.jersey.movies;
+
+public class Movie {
+    public void addMovie(){
+        //add movie entry into the database, the file into the ningx, and convert it into low resolution
+    }
+
+    public void deleteMovie(){
+        //remove movie from the database and delete the files
+    }
+
+    public void getMovie(){
+        //get movie info
+    }
+}
