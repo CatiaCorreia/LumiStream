0847d0a988afbcb9247f6d99ae566467f1e40fb9
fanimoreiraneto <146326937+fanimoreiraneto@users.noreply.github.com>
2024-12-12 13:52:09 +0000

    Update Movie.java

--- src/main/java/com/lumistream/jersey/movies/Movie.java
+++ src/main/java/com/lumistream/jersey/movies/Movie.java
@@ -1,34 +1,34 @@
 package com.lumistream.jersey.movies;
 
 public class Movie {
-    Integer movie_id;
+    Integer movieId;
     String title;
     String description;
     String genre;
-    Integer release_year;
-    String video_url_mp4;
-    String video_url_360p;
-    String video_url_1080p;
+    Integer releaseYear;
+    String videoUrlMp4;
+    String videoUrl360p;
+    String videoUrl1080p;
     Double duration;
     Double rating;
 
-    public Movie(Integer movie_id, String title, String description, String genre, Integer release_year,
-                 String video_url_mp4, String video_url_360p, String video_url_1080p,
+    public Movie(Integer movieId, String title, String description, String genre, Integer releaseYear,
+                 String videoUrlMp4, String videoUrl360p, String videoUrl1080p,
                  Double duration, Double rating) {
-        this.movie_id = movie_id != null ? movie_id : 0;
+        this.movieId = movieId != null ? movieId : 0;
 	    this.title = title != null ? title : "";
         this.description = description != null ? description : "";
         this.genre = genre != null ? genre : "Unknown";
-        this.release_year = release_year != null ? release_year : 0;
-        this.video_url_mp4 = video_url_mp4 != null ? video_url_mp4 : "default_video_url_mp4";
-        this.video_url_360p = video_url_360p != null ? video_url_360p : "default_video_360p_url";
-        this.video_url_1080p = video_url_1080p != null ? video_url_1080p : "default_video_1080p_url";
+        this.releaseYear = releaseYear != null ? releaseYear : 0;
+        this.videoUrlMp4 = videoUrlMp4 != null ? videoUrlMp4 : "defaultVideoUrlMp4";
+        this.videoUrl360p = videoUrl360p != null ? videoUrl360p : "defaultVideoUrl360p";
+        this.videoUrl1080p = videoUrl1080p != null ? videoUrl1080p : "defaultVideoUrl1080p";
         this.duration = duration != null ? duration : 0.0;
         this.rating = rating != null ? rating : 0.0;
     }
 
     public Integer getMovieId() {
-        return movie_id;
+        return movieId;
     }
 
     public String getTitle() {
@@ -44,19 +44,19 @@ public class Movie {
     }
 
     public Integer getReleaseYear() {
-        return release_year;
+        return releaseYear;
     }
 
     public String getVideoUrlMp4() {
-        return video_url_mp4;
+        return videoUrlMp4;
     }
 
     public String getVideoUrl360p() {
-        return video_url_360p;
+        return videoUrl360p;
     }
 
     public String getVideoUrl1080p() {
-        return video_url_1080p;
+        return videoUrl1080p;
     }
 
     public Double getDuration() {
